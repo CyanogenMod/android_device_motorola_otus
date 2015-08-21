8e5f5a857f60d51267847b6f953ef7bbcbb87f51
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-08-20 22:28:24 -0400

    audio: move configs to dedicated directory

--- device.mk
+++ device.mk
@@ -98,6 +98,11 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8610 \
