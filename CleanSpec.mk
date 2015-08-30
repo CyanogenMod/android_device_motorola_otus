03ded30c03d6ef9d9fb55033791169dd7a1f3a8c
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-08-30 00:59:50 -0400

    otus: update copyrights

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,4 +1,4 @@
-# Copyright Copyright (C) Prashant Gahlot (proxthehacker@gmail.com)
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
