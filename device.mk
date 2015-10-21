b68fce979591faec16e31c0ab06cd212dbdfaf40
h2o64 <theh2o64@gmail.com>
2015-10-21 19:48:33 +0200

    otus: Preload BoringSSL compatibility library
    * Required for camera
    
    Change-Id: I894feedb48006910d3ab3f2030352bfe330d12e1

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8610 \
-    libmm-qcamera
+    libmm-qcamera \
+    libboringssl-compat
 
 # Filesystem
 PRODUCT_PACKAGES += \
