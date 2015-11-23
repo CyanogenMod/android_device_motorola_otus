be495664378c411a363a5f3f9c810620a2893932
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-11-22 23:11:52 -0500

    otus: update FM for M
    Change-Id: I28967f2cefdab5f50c452af2ebb0b48a15f858bf

--- device.mk
+++ device.mk
@@ -157,10 +157,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # Lights
 PRODUCT_PACKAGES += \
