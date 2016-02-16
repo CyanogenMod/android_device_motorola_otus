04ba61db8bedfaf73877332d57da9bc9a5034a02
Ashwin Ramesh <ashwinr64@gmail.com>
2016-02-16 04:21:40 -0800

    otus: Commonize Audio Configuration [2/2]
    Change-Id: Ib89b4716595c86fe2044db9b82e6ed2f760155f0

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,12 +31,6 @@ $(call inherit-product-if-exists, vendor/motorola/otus/otus-vendor.mk)
 # Prebuilt
 PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,${LOCAL_PATH}/prebuilt/system,system)
-
-# Audio configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/overlay
