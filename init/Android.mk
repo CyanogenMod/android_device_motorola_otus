d85d4d64a88b99b6fe4e5ceb861ac4c2b8c2a5b8
Ashwin Ramesh <ashwinr64@gmail.com>
2016-05-11 03:49:27 -0700

    otus: init: Fix Typo
    Change-Id: Ia0a7dd3ab3fb8b2b5d4e26fed9300bd7e5be1f72

--- init/Android.mk
+++ init/Android.mk
@@ -21,7 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_condor.cpp
-LOCAL_MODULE := libinit_condor
+LOCAL_SRC_FILES := init_otus.cpp
+LOCAL_MODULE := libinit_otus
 
 include $(BUILD_STATIC_LIBRARY)
